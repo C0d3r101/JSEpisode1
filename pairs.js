4de805420e9438a45d7cf5a5b1de49b8d8add76e
Abdullah Yousef <ayo@Abdullahs-MacBook-Air.local>
2018-03-28 05:12:12 +0300

    ???

--- pairs.js
+++ pairs.js
@@ -17,9 +17,36 @@
 * - It returns an empty array if it gets passed nothing:
 *       pairs() returns []
 ****************************************************************/
-function pairs(names) {
-  // Your code goes here
+Array.prototype.getRandom = function () {
+  return this.splice(Math.floor(Math.random()*this.length), 1)[0];
 }
+
+function pairs(names) {
+	let names2 = names // a copy of the provided array to manipulate with affecting original one
+	let random =[];
+	 
+while (names2.length !== 0){
+	
+	if(name2.length>1){
+			namesRandomizing = names2.getRandom()
+			random.push(namesRandomizing);
+			/*i = names2.indexOf(namesRandomizing);
+			names2.splice(i,1);*/
+			if (random.length === 2) { break; }
+			pairs.push(random);
+			random.splice(0,random.length);
+			
+			if (random.length !== 2 && random.length !== 0){
+				continue;
+			}
+	
+	else if (names2.length == 1) {
+			pairs.push(random);
+			random.splice(0,random.length);
+			}
+
+	else {return [];}
+	}
 
 export default pairs;
 
